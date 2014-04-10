867fafec56b2f56c07ab0d0350d5ea7871cdb482
manbensam <abdulrahmanpancasila@gmail.com>
2014-04-10 17:40:38 +0700

    Pemrog01RegulerGenap1314

--- P1/src/sim.java
+++ P1/src/sim.java
@@ -5,21 +5,21 @@ package Praktikum03c.bin;
 
 public class sim
 {
-	public static void main(String[] Abdulrahman)
+	public static void main(String[] Riandi)
 	{
-		System.out.println("Nama					: ABDUL RAHMAN");
-		System.out.println("Alamat					: JALAN LINTAS SELATAN KEDAMIN HULU PTS SELATAN KAPUAS HULU");
-		System.out.println("Tempat/Tanggal Lahir	: KP.JATI,16-05-1992");
+		System.out.println("Nama					: Riandi Rachman");
+		System.out.println("Alamat					: JALAN RAYA GAS ALAM KOMP KOPERASI C 105");
+		System.out.println("Tempat/Tanggal Lahir	: Jakarta,10-07-1992");
 		System.out.println("Tinggi					: 165 cm");
 		System.out.println("Jenis Kelamin			:LAKI-LAKI");
 		System.out.println("Pekerjaan				:PELAJAR/MAHASISWA");
-		System.out.println("No.SIM					:920510191014");
-		System.out.println("Berlaku s/d				:16-05-2017");
+		System.out.println("No.SIM					:920712210749");
+		System.out.println("Berlaku s/d				:18-07-2017");
 		System.out.println("");
-		System.out.println("PUTUSSIBAU,21-05-2012");
+		System.out.println("MULYADI,02-01-2012");
 		System.out.println("KAPOLRES");
 		System.out.println("");
-		System.out.println("DHANI KRISTIANTO,SIK");
-		System.out.println("AKBP.NRP 71120455");
+		System.out.println("MULYADI,MSI");
+		System.out.println("KOMBES.POL 65040680");
 	}
 }
